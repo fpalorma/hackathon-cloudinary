9fbbe44985062bef4110b0120ccdc8f69a951f99
Federico Palorma <140191203+fpalorma@users.noreply.github.com>
2024-10-21 00:05:39 -0300

    Styles (#1)
    * first branch commit
    
    * added styles and loader

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+<div className="absolute inset-0 top-0 z-[-2] min-h-screen w-full bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>        {children}
       </body>
     </html>
   );
